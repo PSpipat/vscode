870d8630fdd3bd339af97ce20f74868573704dd9
PSpipat <pipat8523@gmail.com>
2022-06-16 11:00:54 +0700

    first commit

---
+++ lab1.py
@@ -0,0 +1,21 @@
+print('hello Rmutsb Sci cs ITS 16541N')
+n = 10
+if n == 10 :
+   print('n equal to 10')
+else:
+   print('n is something else except 10')
+level = input('Enter level (1 - 4): ')
+if level == '1':
+   print('Easy')
+elif level == '2':
+   print('Medium')
+elif level == '3':
+   print('Hard')
+elif level == '4':
+  print('Expert')
+else:
+  print('Invalid level selected')
+
+
+
+
