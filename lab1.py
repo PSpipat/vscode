bac55880ab660d6b202e42366d10993ff0c1c57a
PSpipat <pipat8523@gmail.com>
2022-06-16 11:18:46 +0700

    new commit

--- lab1.py
+++ lab1.py
@@ -9,22 +9,7 @@ if n == 10 :
 else:
    print('n is something else except 10')
 #///////////////////////////
-level = input('Enter level (1 - 4): ')
-if level == '1':
-   print('Easy')
-elif level == '2':
-   print('Medium')
-elif level == '3':
-   print('Hard')
-elif level == '4':
-  print('Expert')
-else:
-  print('Invalid level selected')
-#/////////////////
-count = 1
-while count <= 8:
-   print('I like Rmutsb sci')
-   count = count+1
+
 
 
 
