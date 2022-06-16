58d40b5b44441671e2069011aa7972562221abd3
PSpipat <pipat8523@gmail.com>
2022-06-16 11:15:38 +0700

    new commit

--- lab1.py
+++ lab1.py
@@ -1,9 +1,14 @@
+from itertools import count
+
+
 print('hello Rmutsb Sci cs ITS 16541N')
+#///////////////////////////
 n = 10
 if n == 10 :
    print('n equal to 10')
 else:
    print('n is something else except 10')
+#///////////////////////////
 level = input('Enter level (1 - 4): ')
 if level == '1':
    print('Easy')
@@ -15,7 +20,11 @@ elif level == '4':
   print('Expert')
 else:
   print('Invalid level selected')
-
+#/////////////////
+count = 1
+while count <= 8:
+   print('I like Rmutsb sci')
+   count = count+1
 
 
 
